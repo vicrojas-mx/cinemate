625dcdc46e98cbc4a86c311ed795e0fca9d5d379
Victor Rojas <vicrojas_mx@hotmail.com>
2024-04-08 08:16:04 -0400

    Updating comment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { ScrollToTop } from './components';
 import './index.css';
 import App from './App';
 
+// New comment 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
